c282a8df9099e9b8ae68f5f0d409752dee83feb6
reekaze <tan.albert53@gmail.com>
2024-02-13 06:32:13 +0700

    update hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,9 +5,9 @@ type Props = {};
 
 const Hero = ({}: Props) => {
   return (
-    <div className="flex relative overflow-hidden w-full min-h-[200px] max-h-[500px] h-[50vw]">
-      <div className="absolute top-[15%] sm:top-[40%] left-[5%] w-2/5 md:w-1/3 z-10 p-2 md:p-4 bg-slate-600/50 backdrop-blur-sm rounded-md">
-        <h3 className="text-p sm:text-h3 font-bold text-white">
+    <div className="flex relative overflow-hidden w-full min-h-[300px] max-h-[500px] h-[50vw]">
+      <div className="absolute top-[30%] sm:top-[40%] left-[5%] w-2/5 md:w-1/3 z-10 p-2 md:p-4 bg-slate-600/50 backdrop-blur-sm rounded-md">
+        <h3 className=" text-p sm:text-h3 font-bold text-white">
           Elevate Your Digital Experience with Great Laptop Quality.
         </h3>
       </div>
