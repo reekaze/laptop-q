a173505f7cc47933906d70b7995bc4bd5bf4b896
reekaze <tan.albert53@gmail.com>
2024-02-20 10:20:33 +0700

    fix bug add product images

--- src/components/products/add/ImagesInput.tsx
+++ src/components/products/add/ImagesInput.tsx
@@ -3,7 +3,7 @@ import { Separator } from "@/components/ui/separator";
 import { toast } from "@/components/ui/use-toast";
 import { ImageIcon, PlusIcon, X } from "lucide-react";
 import Image from "next/image";
-import React, { Dispatch, SetStateAction, useRef } from "react";
+import React, { useRef } from "react";
 
 type ImagesInputProps = {
   images: string[];
@@ -15,7 +15,7 @@ const ImagesInput = ({ images, onChange, max }: ImagesInputProps) => {
   const inputRef = useRef<HTMLInputElement | null>(null);
   return (
     <div className="flex flex-col gap-4">
-      <div className="font-semibold">Images</div>
+      <div className="font-semibold">Imagess</div>
       <Separator />
       <div className="relative flex flex-row p-4 gap-4 bg-green-300/50 rounded-md overflow-x-scroll">
         {images.map((image, idx) => {
@@ -45,12 +45,12 @@ const ImagesInput = ({ images, onChange, max }: ImagesInputProps) => {
             accept="image/*"
             ref={inputRef}
             className="file:hidden w-20 h-20 rounded-xl bg-transparent text-transparent cursor-pointer"
+            value={""}
             onChange={(event) => {
               const file = event?.target?.files?.[0];
               if (file) {
                 if (images.length < (max || 4)) {
                   onChange([...images, URL.createObjectURL(file)]);
-                  console.log(images[0]);
                 } else {
                   toast({
                     description: `You can only upload ${max || 4} images`,
